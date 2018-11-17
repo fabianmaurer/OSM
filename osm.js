8a97a0e18f8785b97e724f8d5903d0c61c519a4d
Fabian Maurer <10630481+fabianmaurer@users.noreply.github.com>
2018-11-17 11:33:01 +0100

    moved js from html to .js

--- osm.js
+++ osm.js
@@ -1,19 +1,45 @@
-pbfParser.parse({
-    filePath: 'germany-latest.osm.pbf',
-    endDocument: function(){
-        console.log('document end');
-    },
-    node: function(node){
-        console.log('node: ' + JSON.stringify(node));
-    },
-    way: function(way){
-        console.log('way: ' + JSON.stringify(way));
-    },
-    relation: function(relation){
-        console.log('relation: ' + JSON.stringify(relation));
-    },
-    error: function(msg){
-        console.error('error: ' + msg);
-        throw msg;
-    }
-});
+var logEle = document.getElementById("log");
+            
+            function log(msg) {
+                logEle.textContent += msg + '\n';
+            }
+
+            function parse(file) {
+                var cNodes = 0,
+                    cWays = 0,
+                    cRels = 0;
+
+                log('parsing "' + file.name + '" ...');
+
+                pbfParser.parse({
+                    file: file,
+                    endDocument: function(){
+                        log('done.\n');
+                        log('nodes: ' + cNodes);
+                        log('ways:  ' + cWays);
+                        log('rels:  ' + cRels + '\n');
+                    },
+                    bounds: function(bounds){
+                    },
+                    node: function(node){
+                        cNodes++;
+                    },
+                    way: function(way){
+                        cWays++;
+                    },
+                    relation: function(relation){
+                        cRels++;
+                    },
+                    error: function(msg){
+                        log('error: ' + msg);
+                        throw msg;
+                    }
+                });
+            }
+
+            function handleFile() {
+                var file = this.files[0];
+                parse(file);
+            }
+
+            document.getElementById("file").addEventListener("change", handleFile, false);
