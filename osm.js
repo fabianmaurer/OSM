1aaf6e773daea8d9d9318879325e58b625d5722c
Fabian Maurer <10630481+fabianmaurer@users.noreply.github.com>
2018-11-24 15:11:07 +0100

    way better now

--- osm.js
+++ osm.js
@@ -8,19 +8,107 @@ function log(msg) {
     logEle.textContent += msg + '\n';
 }
 
+function fillTable(entries){
+    let $content=$('.popup-content');
+    let $tables=$(document.createElement('table'));
+    $tables.html('<tr><td>nodes</td><td>edges</td></tr>');
+    $tables.css('text-align','center')
+    $tables.css('table-layout','fixed');
+    $content.append($tables);
+    $nodeTableContainer=$(document.createElement('td'));
+    $nodeTableContainer.css('border-right','1px solid #333');
+    $nodeTableContainer.css('width','50%');
+    $tables.append($nodeTableContainer);
+    let $nodeTable=$(document.createElement('table'));
+    $nodeTable.html('<td>id</td><td>lat</td><td>lon</td>');
+    for(let i=0;i<Math.min(entries,nodes.length);i++){
+        $nodeTable.append('<tr><td>'+nodes[i].id+'</td><td>'+nodes[i].lat+'</td><td>'+nodes[i].lon+'</td></tr>');
+    }
+    $nodeTableContainer.append($nodeTable);
+    $edgeTableContainer=$(document.createElement('td'));
+    $tables.append($edgeTableContainer);
+    let $edgeTable=$(document.createElement('table'));
+    $edgeTable.html('<td>index</td><td>from node</td><td>to node</td><td>distance (m)</td>');
+    for(let i=0;i<Math.min(entries,edges.length);i++){
+        console.log(edges[i])
+        $edgeTable.append('<tr><td>'+i+'</td><td>'+edges[i].from+'</td><td>'+edges[i].to+'</td><td>'+Math.round(edges[i].distance*1000)+'</td></tr>');
+    }
+    $edgeTableContainer.append($edgeTable);
+    $('.popup-show').show();
+}
+$('.popup-close').click(function(){
+    $('.overlay').fadeOut();
+})
+
+$('.popup-show').click(function(){
+    $('.overlay').fadeIn();
+})
+
 function initmap() {
+    fillTable(100);
     // set up the map
-    map = new L.Map('map',{renderer:L.canvas()});
+    map = new L.Map('map',{renderer:L.canvas(),zoomControl:false});
 
     // create the tile layer with correct attribution
     var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     var osmAttrib = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
     var osm = new L.TileLayer(osmUrl, { minZoom: 8, maxZoom: 22, attribution: osmAttrib });
     const polylineOptions={color:'red',smoothFactor:2.0};
+    let routeFrom=[];
+    let routeTo=[];
+    let fromSelected=false;
+    let fromMarker;
+    let toMarker;
 
     // start the map in South-East England
-    map.setView(new L.LatLng(53, 8.8), 16);
+    map.setView(new L.LatLng(53.0752, 8.8067), 16);
     map.addLayer(osm);
+
+    $('.route-from-close').click(function(){
+        map.removeLayer(fromMarker);
+        fromMarker=null;
+        $('.route-from').addClass('empty');
+        $('.route-from').empty();
+        fromSelected=false;
+        $('.route-from-close').hide();
+    })
+
+    $('.route-to-close').click(function(){
+        map.removeLayer(toMarker);
+        toMarker=null;
+        $('.route-to').addClass('empty');
+        $('.route-to').empty();
+        $('.route-to-close').hide();
+    })
+
+    
+
+    map.on('click', function(e) { 
+        if(!fromSelected){
+            routeFrom=[e.latlng.lat,e.latlng.lng];
+            $('.route-from').html('lat:'+Math.round(e.latlng.lat*1000)/1000+' lng:'+Math.round(e.latlng.lng*1000)/1000);
+            $('.route-from').removeClass('empty');
+            $('.top-left').fadeIn();
+            fromSelected=true;
+            if(fromMarker==null) fromMarker=L.marker([e.latlng.lat,e.latlng.lng],{draggable:true}).addTo(map);
+            else fromMarker.setLatLng(e.latlng);
+            $(fromMarker._icon).css('filter','hue-rotate(270deg)');
+            $('.route-from-close').show();
+        }else{
+            routeTo=[e.latlng.lat,e.latlng.lng];
+            $('.route-to').html('lat:'+Math.round(e.latlng.lat*1000)/1000+' lng:'+Math.round(e.latlng.lng*1000)/1000);
+            $('.route-to').removeClass('empty');
+            if(toMarker==null) toMarker=L.marker([e.latlng.lat,e.latlng.lng],{draggable:true}).addTo(map);
+            else toMarker.setLatLng(e.latlng);
+            $('.route-to-close').show();
+        }
+        // var requestform = e.latlng;
+        // var formpopup = L.popup()
+        // .setLatLng(requestform)
+        // .setContent('lat: ' + e.latlng.lat + 
+        //             'lon: '+e.latlng.lng) 
+        // .openOn(map);
+    });
 
     // create a red polyline from an array of LatLng points
     let zoom1=[];
@@ -41,7 +129,16 @@ function initmap() {
             }
             
         }
-        L.polyline(polylinePoints, polylineOptions).addTo(map);
+        let p=L.polyline(polylinePoints, polylineOptions);
+        if(categoriesZoom1.includes(ways[i].tags.highway)){
+            zoom1.push(p);
+        }
+        else if(categoriesZoom2.includes(ways[i].tags.highway)){
+            zoom2.push(p);
+        }
+        else if(categoriesZoom3.includes(ways[i].tags.highway)){
+            zoom3.push(p);
+        }
         // if(categoriesZoom1.includes(edges[i].tags.highway)){
         //     var latlngs = [
         //         [nodes[nodeIdMap[edges[i].from]].lat, nodes[nodeIdMap[edges[i].from]].lon],
@@ -70,42 +167,15 @@ function initmap() {
         
         // zoom the map to the polyline
         // map.fitBounds(polyline.getBounds());
-    }
-    for(let i=0;i<edges.length;i++){
-        // if(categoriesZoom1.includes(edges[i].tags.highway)){
-        //     var latlngs = [
-        //         [nodes[nodeIdMap[edges[i].from]].lat, nodes[nodeIdMap[edges[i].from]].lon],
-        //         [nodes[nodeIdMap[edges[i].to]].lat, nodes[nodeIdMap[edges[i].to]].lon]
-        //     ];
-        //     let polyline=L.polyline(latlngs, polylineOptions).bindPopup('distance:'+edges[i].distance);
-        //     zoom1.push(polyline);
-        // }
-        // else if(categoriesZoom2.includes(edges[i].tags.highway)){
-        //     var latlngs = [
-        //         [nodes[nodeIdMap[edges[i].from]].lat, nodes[nodeIdMap[edges[i].from]].lon],
-        //         [nodes[nodeIdMap[edges[i].to]].lat, nodes[nodeIdMap[edges[i].to]].lon]
-        //     ];
-        //     let polyline=L.polyline(latlngs, polylineOptions).bindPopup('distance:'+edges[i].distance);
-        //     zoom2.push(polyline);
-        // }
-        // else if(categoriesZoom3.includes(edges[i].tags.highway)){
-        //     var latlngs = [
-        //         [nodes[nodeIdMap[edges[i].from]].lat, nodes[nodeIdMap[edges[i].from]].lon],
-        //         [nodes[nodeIdMap[edges[i].to]].lat, nodes[nodeIdMap[edges[i].to]].lon]
-        //     ];
-        //     let polyline=L.polyline(latlngs, polylineOptions).bindPopup('distance:'+edges[i].distance);
-        //     zoom3.push(polyline);
-        // }
-        
-        
-        // zoom the map to the polyline
-        // map.fitBounds(polyline.getBounds());
+    
     }
 
     let layer1=L.layerGroup(zoom1);
         let layer2=L.layerGroup(zoom2);
         let layer3=L.layerGroup(zoom3);
         map.addLayer(layer1)
+        map.addLayer(layer2)
+        map.addLayer(layer3)
 
         map.on('zoomend', function() {
             console.log(map.getZoom())
@@ -125,7 +195,7 @@ function initmap() {
                     map.removeLayer(layer3);
                 }
             }
-            if (map.getZoom() >= 14){
+            if (map.getZoom() >= 13){
                 if (!map.hasLayer(layer2)){
                     map.addLayer(layer2)
                 }
